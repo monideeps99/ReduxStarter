7af8e8737f0a5b64e1f920f98abcf2b4a3105b70
Monideep <monideep.purkayastha@livquik.com>
2017-04-03 09:32:14 +0000

    This commit adds redux to the boilerplate project generated by create-react-app

---
+++ README.md
@@ -0,0 +1,3 @@
+## Geodrop
+
+Discover the best places to visit based on your budget and interests.
---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+        <h3> Geodrop </h3>
+        <p> Discover the best places to travel based on your budget and interests. </p> 
+      </div>
+    )
+  }
+}
